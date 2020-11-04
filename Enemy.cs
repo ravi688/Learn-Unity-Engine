7595910c081ed8e3bcc4ea79e23a361ddeae1538
Ravi Prakash Singh <rp0412204@gmail.com>
2020-11-04 05:02:52 -0800

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed = 10.0f;
+    public float rotation_speed = 4.0f;
+
+
+    private GameObject target; 
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Target"); 
+    }
+
+    private void Update()
+    {
+
+        Vector2 direction = (target.transform.position - this.transform.position);
+
+        if (direction.sqrMagnitude >= 0.5f)
+        {
+            float sign = Mathf.Sign(Vector3.Dot(Vector3.Cross(transform.up, direction), Vector3.forward));
+            float angle = Vector2.Angle(direction, transform.up) * sign * rotation_speed * Time.deltaTime;
+            transform.rotation *= Quaternion.AngleAxis(angle, Vector3.forward);
+
+            transform.position += (Vector3)direction.normalized * Time.deltaTime * speed;
+        }
+    }
+	
+
+}
---
+++ Target.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour {
+
+    public Vector2 top_right_corner;
+    public Vector2 bottom_left_corner;
+
+
+    private Collider2D[] results; 
+
+    private void Start()
+    {
+        results = new Collider2D[6]; 
+    }
+
+    private void Update()
+    {
+
+        int num_colliders = Physics2D.OverlapAreaNonAlloc(top_right_corner, bottom_left_corner, results);
+
+        for (int i = 0; i < num_colliders; i++)
+        {   
+            Debug.Log(results[i].gameObject.name); 
+        }
+
+    }
+
+
+
+    private void OnDrawGizmos()
+    {
+
+        CustomDebug.DrawRectange(top_right_corner, bottom_left_corner); 
+
+    }
+
+
+}
